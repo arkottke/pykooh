4589e8d377c7a925ff8450a7634ad383cabdc692
Albert Kottke <albert.kottke@gmail.com>
2019-04-29 10:22:25 -0700

    Fixed pep8 issues.

--- tests/test_konno_ohmachi.py
+++ tests/test_konno_ohmachi.py
@@ -7,7 +7,7 @@ import cyko
 DATA_PATH = pathlib.Path(__file__).parent / 'data'
 
 # Load test data
-data =np.load(str(DATA_PATH / 'test_data.npz'))
+data = np.load(str(DATA_PATH / 'test_data.npz'))
 freqs = data['freqs']
 raw_amps = data['fourier_amps']
 smooth_amps = data['ko_amps']
