d1ee63767826bc9c78616f0eea3721c4a4937020
Albert Kottke <albert.kottke@gmail.com>
2019-05-15 10:55:33 -0700

    Add method for effective amplitude spectrum.

--- cyko/__init__.py
+++ cyko/__init__.py
@@ -1,6 +1,10 @@
 """cyko - Cython implementation of the Konno Ohmachi."""
 
+import numpy as np
+
 from pkg_resources import get_distribution
+
+from .konno_ohmachi import smooth
 
 __author__ = 'Albert Kottke'
 __copyright__ = 'Copyright 2019 Albert Kottke'
@@ -9,6 +13,72 @@ __title__ = 'cyko'
 __version__ = get_distribution('cyko').version
 del get_distribution
 
-from .konno_ohmachi import smooth
 
+def effective_ampl(freqs, fourier_amps_h1, fourier_amps_h2, freqs_ea=None,
+                   missing='zero'):
+    """
+    Compute the effective amplitude spectrum (EAS) as defined in Kottke et al.
+    (2018).
 
+    Parameters
+    ----------
+    freqs: `array_like`
+        Frequencies of the Fourier amplitude spectra
+    fourier_amps_h1: `array_like`
+        Fourier amplitude spectrum of the first horizontal component.
+    fourier_amps_h2: `array_like`
+        Fourier amplitude spectrum of the second horizontal component.
+    freqs_ea: `array_like` (optional)
+        Frequencies at which the EAS should be computed. If *None*, the
+        frequencies of the EAS will be computed based on the provided frequency
+        range. The resulting frequencies are log-space with 100 points per
+        decade and start at the start of the decade.
+    missing: str (optional)
+        Treatment of missing values. Options are:
+            'zero' - use 0 for missing values (default)
+            'nan' - use `np.nan` for missing values
+            'trim' - only provide EAS over the range of continuous values
+    Returns
+    -------
+    freqs_ea: :class:`np.ndarray`
+        Frequency of the spectrum
+    effect_amp: :class:`np.ndarray`
+        Effective amplitude spectrum.
+
+    """
+
+    # Calculate the average power of the two components
+    avg = np.abs(np.sqrt(0.5 * (fourier_amps_h1 ** 2 + fourier_amps_h2 ** 2)))
+
+    if freqs_ea is None:
+        # Frequency range is 100 points per decades
+        start = 1 if np.isclose(freqs[0], 0) else 0
+        left = np.floor(np.log10(freqs[start])).astype(int)
+        right = np.ceil(np.log10(freqs[-1])).astype(int)
+        ndecades = right - left
+        freqs_ea = np.logspace(left, right, num=(1 + 100 * ndecades))
+
+        # Only compute over the known frequency range. This process is done
+        # such that the freqs_ko are exact at the start of the decade (e.g.,
+        # 0.1 1, 10, etc.)
+        mask = (freqs[start] < freqs_ea) & (freqs_ea < freqs[-1])
+        freqs_ea = freqs_ea[mask]
+
+    # Smooth the average spectrum using b of 188.5, which provides a smoothing
+    # operator with a bandwidth of 1/30 of a decade
+    smoothed = smooth(freqs_ea, freqs, avg, 188.5)
+
+    if missing == 'nan':
+        smoothed[np.isclose(smoothed, 0)] = np.nan
+    elif missing == 'trim':
+        # First index with continuously defined values
+        first = np.where(np.isclose(smoothed, 0))[0][-1] + 1
+        freqs_ea = freqs_ea[first:]
+        smoothed = smoothed[first:]
+    elif missing == 'zero':
+        # Default behavior
+        pass
+    else:
+        raise NotImplementedError
+
+    return freqs_ea, smoothed
---
+++ tests/test_init.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pytest
+
+import cyko
+
+from . import DATA_PATH
+
+
+def read_at2(fname):
+    with open(fname) as fp:
+        for _ in range(3):
+            next(fp)
+        time_step = float(next(fp).split()[3])
+        accels = np.array([p for l in fp for p in l.split()]).astype(float)
+    return time_step, accels
+
+
+@pytest.fixture()
+def fourier_spectra():
+    time_step, accels_h1 = read_at2(DATA_PATH / 'RSN4863_CHUETSU_65036EW.AT2')
+    accels_h2 = read_at2(DATA_PATH / 'RSN4863_CHUETSU_65036NS.AT2')[1]
+    accels = np.c_[accels_h1, accels_h2]
+
+    fourier_amps = np.fft.rfft(accels, axis=0)
+    freqs = np.fft.rfftfreq(accels.shape[0], d=time_step)
+
+    return freqs, fourier_amps
+
+
+@pytest.mark.parametrize('missing', ['zero', 'nan', 'trim'])
+def test_effective_ampl(missing, fourier_spectra):
+    freqs, fourier_amps = fourier_spectra
+    # FIXME Add test
+    freqs_ea, eff_ampl = cyko.effective_ampl(
+        freqs, fourier_amps[:, 0], fourier_amps[:, 1], missing=missing)
