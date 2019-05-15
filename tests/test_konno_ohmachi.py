91dcef4b7fe784069bff53c676383b00db9352f9
Albert Kottke <albert.kottke@gmail.com>
2019-05-15 10:55:53 -0700

    Refactor for data path.

--- tests/test_konno_ohmachi.py
+++ tests/test_konno_ohmachi.py
@@ -1,19 +1,17 @@
-import pathlib
-
 import numpy as np
 
 import cyko
 
-DATA_PATH = pathlib.Path(__file__).parent / 'data'
+from . import DATA_PATH
 
 # Load test data
 data = np.load(str(DATA_PATH / 'test_data.npz'))
 freqs = data['freqs']
 raw_amps = data['fourier_amps']
 smooth_amps = data['ko_amps']
-bandwidth = data['bandwidth']
+b = data['b']
 
 
 def test_smooth():
-    calculated = cyko.smooth(freqs, freqs, raw_amps, bandwidth)
+    calculated = cyko.smooth(freqs, freqs, raw_amps, b)
     np.testing.assert_allclose(calculated, smooth_amps, rtol=1E-3)
